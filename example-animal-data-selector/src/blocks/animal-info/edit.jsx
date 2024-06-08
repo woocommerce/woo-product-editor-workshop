975fab0e6204a9cc6e723613e9bfe342849a6a6c
Damián Suárez <rdsuarez@gmail.com>
2024-06-08 11:21:24 +0100

    eslinting

--- example-animal-data-selector/src/blocks/animal-info/edit.jsx
+++ example-animal-data-selector/src/blocks/animal-info/edit.jsx
@@ -1,41 +1,21 @@
 /**
- * Retrieves the translation of text.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
+ * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-
-/**
- * React hook that is used to mark the block wrapper element.
- * It provides all the necessary props like the class name.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
- */
 import { useBlockProps } from '@wordpress/block-editor';
 import { createElement } from '@wordpress/element';
-
 /**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
- * Those files can contain any CSS code that gets applied to the editor.
- *
- * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ * Internal dependencies
  */
 import './editor.scss';
 
-/**
- * The edit function describes the structure of your block in the context of the
- * editor. This represents what the editor will render when the block is used.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
- *
- * @return {Element} Element to render.
- */
 export default function Edit() {
 	return (
 		<div { ...useBlockProps() }>
 			<h3>{ __( 'Animal Info', 'animal-info' ) }</h3>
 			<p>
-				Animal Type: <i>Type</i><br />
+				Animal Type: <i>Type</i>
+				<br />
 				Animal Age: <i>Age</i>
 			</p>
 		</div>
