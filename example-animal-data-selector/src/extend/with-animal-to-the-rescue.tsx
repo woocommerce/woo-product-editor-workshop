97642aeff16f111b2e0219543b2fd57141a03a19
Damián Suárez <rdsuarez@gmail.com>
2024-06-12 19:17:31 +0200

    eslint issues

--- example-animal-data-selector/src/extend/with-animal-to-the-rescue.tsx
+++ example-animal-data-selector/src/extend/with-animal-to-the-rescue.tsx
@@ -6,38 +6,41 @@ import { __ } from '@wordpress/i18n';
 import { Button } from '@wordpress/components';
 import { Icon, bug } from '@wordpress/icons';
 import { createHigherOrderComponent } from '@wordpress/compose';
-import { __experimentalSectionActions as SectionActions, __experimentalUseProductEntityProp as useProductEntityProp } from '@woocommerce/product-editor';
+import {
+	__experimentalSectionActions as SectionActions,
+	__experimentalUseProductEntityProp as useProductEntityProp,
+} from '@woocommerce/product-editor';
 import { useEntityProp } from '@wordpress/core-data';
 
 function SuggestionButton() {
-    const [ animalType ] = useProductEntityProp< string >(
-        'meta_data.animal_type',
-        {
-            postType: 'product',
-            fallbackValue: '',
-        }
-    );
-    const [ , setDescription ] = useEntityProp(
-        'postType',
-        'product',
-        'description'
-    );
+	const [ animalType ] = useProductEntityProp< string >(
+		'meta_data.animal_type',
+		{
+			postType: 'product',
+			fallbackValue: '',
+		}
+	);
+	const [ , setDescription ] = useEntityProp(
+		'postType',
+		'product',
+		'description'
+	);
 
-    function suggestDescription() {
-        if ( ! animalType ) {
-            return;
-        }
-    
-        setDescription(
-            `This product is perfect for your ${ animalType.toUpperCase() }!`
-        );
-    }
+	function suggestDescription() {
+		if ( ! animalType ) {
+			return;
+		}
+
+		setDescription(
+			`This product is perfect for your ${ animalType.toUpperCase() }!`
+		);
+	}
 
 	return (
 		<Button
 			variant="secondary"
 			icon={ <Icon icon={ bug } /> }
-            onClick={ suggestDescription }
+			onClick={ suggestDescription }
 		>
 			{ __( 'Need help with this block?', 'woocommerce' ) }
 		</Button>
@@ -45,24 +48,23 @@ function SuggestionButton() {
 }
 
 const withAnimalToTheRescue = createHigherOrderComponent( ( BlockEdit ) => {
-    
 	return ( props ) => {
-        const { name, attributes } = props;
+		const { name, attributes } = props;
 
-        const { _templateBlockId } = attributes;
-        if ( name !== 'woocommerce/product-summary-field' ) {
-            return <BlockEdit { ...props } />;
-        }
-    
-        if ( _templateBlockId !== 'product-description__content' ) {
-            return <BlockEdit { ...props } />;
-        }
+		const { _templateBlockId } = attributes;
+		if ( name !== 'woocommerce/product-summary-field' ) {
+			return <BlockEdit { ...props } />;
+		}
+
+		if ( _templateBlockId !== 'product-description__content' ) {
+			return <BlockEdit { ...props } />;
+		}
 
 		return (
 			<>
 				<SectionActions>
-                    <SuggestionButton />
-                </SectionActions>
+					<SuggestionButton />
+				</SectionActions>
 				<BlockEdit { ...props } />
 			</>
 		);
