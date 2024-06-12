28f729ca56d1589d60cf3a8ca8dd7c31f3e51151
Lourens Schep <lourensschep@gmail.com>
2024-06-12 13:26:50 +0200

    Create suggestion button and modify description on click

--- example-animal-data-selector/src/extend/with-animal-to-the-rescue.tsx
+++ example-animal-data-selector/src/extend/with-animal-to-the-rescue.tsx
@@ -3,9 +3,46 @@
  */
 import React from 'react';
 import { __ } from '@wordpress/i18n';
+import { Button } from '@wordpress/components';
+import { Icon, bug } from '@wordpress/icons';
 import { createHigherOrderComponent } from '@wordpress/compose';
-import { __experimentalSectionActions as SectionActions } from '@woocommerce/product-editor';
+import { __experimentalSectionActions as SectionActions, __experimentalUseProductEntityProp as useProductEntityProp } from '@woocommerce/product-editor';
+import { useEntityProp } from '@wordpress/core-data';
 
+function SuggestionButton() {
+    const [ animalType ] = useProductEntityProp< string >(
+        'meta_data.animal_type',
+        {
+            postType: 'product',
+            fallbackValue: '',
+        }
+    );
+    const [ , setDescription ] = useEntityProp(
+        'postType',
+        'product',
+        'description'
+    );
+
+    function suggestDescription() {
+        if ( ! animalType ) {
+            return;
+        }
+    
+        setDescription(
+            `This product is perfect for your ${ animalType.toUpperCase() }!`
+        );
+    }
+
+	return (
+		<Button
+			variant="secondary"
+			icon={ <Icon icon={ bug } /> }
+            onClick={ suggestDescription }
+		>
+			{ __( 'Need help with this block?', 'woocommerce' ) }
+		</Button>
+	);
+}
 
 const withAnimalToTheRescue = createHigherOrderComponent( ( BlockEdit ) => {
     
@@ -24,7 +61,7 @@ const withAnimalToTheRescue = createHigherOrderComponent( ( BlockEdit ) => {
 		return (
 			<>
 				<SectionActions>
-	                <h1>🐶 knows!</h1>
+                    <SuggestionButton />
                 </SectionActions>
 				<BlockEdit { ...props } />
 			</>
