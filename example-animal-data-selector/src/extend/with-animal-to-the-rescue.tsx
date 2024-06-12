a5b5f0dd82dab95933054ce687fc8bbb377ab310
Lourens Schep <lourensschep@gmail.com>
2024-06-12 13:22:26 +0200

    Only extend the description block

--- example-animal-data-selector/src/extend/with-animal-to-the-rescue.tsx
+++ example-animal-data-selector/src/extend/with-animal-to-the-rescue.tsx
@@ -6,7 +6,19 @@ import { __ } from '@wordpress/i18n';
 import { createHigherOrderComponent } from '@wordpress/compose';
 
 const withAnimalToTheRescue = createHigherOrderComponent( ( BlockEdit ) => {
+    
 	return ( props ) => {
+        const { name, attributes } = props;
+
+        const { _templateBlockId } = attributes;
+        if ( name !== 'woocommerce/product-summary-field' ) {
+            return <BlockEdit { ...props } />;
+        }
+    
+        if ( _templateBlockId !== 'product-description__content' ) {
+            return <BlockEdit { ...props } />;
+        }
+
 		return (
 			<>
 				<h1>🐶 knows!</h1>
